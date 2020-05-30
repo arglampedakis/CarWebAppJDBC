095ba92002eeaf5ad2fe2b9c566c35e2ef6202b6
glamb <glamb@192.168.1.65>
2020-05-30 23:38:41 +0300

    1st commit.

---
+++ CarWebAppJDBC/src/main/java/util/DBUtils.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author glamb
+ */
+public class DBUtils {
+    
+    public Connection getConPool() {
+        Connection con = null;
+        try {
+            //USE JNDI
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:/comp/env");
+
+            DataSource ds = (DataSource) envContext.lookup("jdbc/test");
+            con = ds.getConnection();
+
+        } catch (NamingException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return con;
+    }
+}
---
+++ CarWebAppJDBC/src/main/webapp/newCarDetails.jsp
@@ -0,0 +1,32 @@
+<%-- 
+    Document   : newCarDetails
+    Created on : May 30, 2020, 11:05:04 PM
+    Author     : glamb
+--%>
+
+<%@page import="model.Car"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>New Car</title>
+    </head>
+    <body>
+         <% 
+        if (request.getAttribute("failMessage") != null) {
+                out.print(request.getAttribute("failMessage"));
+            }else{
+        Car car = (Car) request.getAttribute("myCar");
+        if (car != null) {%>
+        <h1>New Car details:</h1>
+        <ul>
+            <%= "<li> ID: " + car.getId() + "</li>"%>
+            <%= "<li> Plate: " + car.getPlate() + "</li>"%>
+            <%="<li> Horsepower: " + car.getHorsepower() + "</li>"%>
+        </ul>
+        <% }}%>
+        <br>
+        <a href="newCarForm.html">Go back</a>
+    </body>
+</html>
